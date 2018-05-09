84657d4ba45e61406b3c6017ec8caf160bc02ef4
Lishu <lishu@hiynn.com>
2018-05-09 14:06:32 +0800

    前端框架待搭建

---
+++ src/common/api.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const urlMap = {
+    signUp: '/api/sign_up',
+    signIn: '/api/sign_in',
+    exit: '/api/exit',
+    loverInfo: '/api/lover_info',
+    updateRelation: '/api/update_relation',
+
+    status: '/api/status',
+    createStatus: '/api/create_status',
+    deleteStatus: '/api/delete_status',
+
+    plan: '/api/plan',
+    createPlan: '/api/create_plan',
+    updatePlan: '/api/update_plan',
+    deletePlan: '/api/delete_plan',
+    
+    anniversary: '/api/anniversary',
+    createAnniversary: '/api/create_anniversary',
+    deleteAnniversary: '/api/delete_anniversary'    
+};
+
+export default function(url, data, method) {
+    switch(method) {
+    case 'get':
+        return axios.get(urlMap[url], {
+            params: data
+        });
+    case 'post':
+        return axios.post(urlMap[url], data);
+    default:
+        return axios.get(urlMap[url], {
+            params: data
+        });
+    }
+}
